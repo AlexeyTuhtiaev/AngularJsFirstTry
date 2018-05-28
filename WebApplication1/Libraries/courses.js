03e267a76fe15af11790e710bb8a21d72cffd956
Alexey <alexey.tuhtiaev@gmail.com>
2018-05-28 18:19:57 +0300

    ng-switch & ng-include

---
+++ WebApplication1/Libraries/courses.js
@@ -0,0 +1,45 @@
+﻿var model = {
+    user: "Alexey",
+    courses: [{ name: "HTML, CSS", passed: true },
+    { name: "JavaScript Essential", passed: true },
+    { name: "JavaScript Advanced", passed: false },
+    { name: "HTML5 &amp; CSS3", passed: true },
+    { name: "AngularJS", passed: true }]
+};
+
+var coursesApp = angular.module("coursesdApp", []);
+
+coursesApp.controller("coursesdAppCtrl", function ($scope) {
+
+    $scope.data = model;
+
+    $scope.addNewCourse = function () {
+        model.courses.push({
+            name: $scope.courseName,
+            passed: false
+        });
+
+        $scope.courseName = "";
+    }
+
+    $scope.convertPassedText = function (passed) {
+        return passed ? "Yes" : "No";
+    }
+
+    $scope.setPassedTextStyle = function (passed) {
+        return passed ? "color: green" : "color: red; font-weit: bold";
+    }
+
+    $scope.tableView = "CoursesTable.html";
+    $scope.shortTableView = "CoursesTableShort.html";
+
+    $scope.url = $scope.tableView;
+
+    $scope.fullTable = function () {
+        $scope.url = $scope.tableView;
+    }
+
+    $scope.shortTable = function () {
+        $scope.url = $scope.shortTableView;
+    }
+});
---
+++ WebApplication1/Libraries/test.js
@@ -0,0 +1,16 @@
+﻿var tests = [{ display: "Question #1", value: 1},
+            { display: "Question #2", value: 2},
+            { display: "Question #3", value: 3 }];
+
+var testModuel = angular.module("testModuel", []);
+
+testModuel.controller("testCtrl", function ($scope) {
+
+    $scope.testItems = tests;
+
+    $scope.mode = 1;
+
+    $scope.onRadioBtnChange = function (value) {
+        $scope.mode = value;
+    }
+});

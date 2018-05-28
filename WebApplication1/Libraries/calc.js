f039ebb2dd543a38c0b92ebfc0854ef29c8621fb
Alexey <alexey.tuhtiaev@gmail.com>
2018-05-28 12:33:31 +0300

    calculator was added

---
+++ WebApplication1/Libraries/calc.js
@@ -0,0 +1,14 @@
+﻿coursesApp.controller("calcCtrl", function ($scope) {
+    $scope.add = function () {
+        $scope.result = parseFloat($scope.firstValue) + parseFloat($scope.secondValue);
+    }
+    $scope.diff = function () {
+        $scope.result = parseFloat($scope.firstValue) - parseFloat($scope.secondValue);
+    }
+    $scope.mult = function () {
+        $scope.result = parseFloat($scope.firstValue) * parseFloat($scope.secondValue);
+    }
+    $scope.div = function () {
+        $scope.result = parseFloat($scope.firstValue) / parseFloat($scope.secondValue);
+    }
+});

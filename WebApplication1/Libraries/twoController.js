c2c029468fec067c1acbf8457bcb07b908b31f75
Alexey <alexey.tuhtiaev@gmail.com>
2018-05-31 21:00:08 +0300

    two different controllers (preparing to work)

---
+++ WebApplication1/Libraries/twoController.js
@@ -0,0 +1,9 @@
+﻿angular.module("twoModule", [])
+    .controller("firstCtrl", function ($scope) {
+        $scope.firstInputValue = "11111";
+    });
+
+angular.module("twoModule")
+    .controller("secondCtrl", function ($scope) {
+        $scope.secondInputValue = "22222";
+    });

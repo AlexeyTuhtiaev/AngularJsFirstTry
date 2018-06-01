9f6662a94211d9e5392d7e7715c8d5d88d2d45ee
Alexey <alexey.tuhtiaev@gmail.com>
2018-06-01 10:26:58 +0300

    sending message using $broadcast and $on

--- WebApplication1/Libraries/twoController.js
+++ WebApplication1/Libraries/twoController.js
@@ -1,9 +1,18 @@
 ﻿angular.module("twoModule", [])
-    .controller("firstCtrl", function ($scope) {
+    .controller("firstCtrl", function ($scope, $rootScope) {
         $scope.firstInputValue = "11111";
+
+        $scope.sendValue = function () {
+            $rootScope.$broadcast("messageFromFirstInput", {
+                messageValue: $scope.firstInputValue});
+        };
     });
 
 angular.module("twoModule")
     .controller("secondCtrl", function ($scope) {
-        $scope.secondInputValue = "22222";
+        //$scope.secondInputValue = "22222";
+
+        $scope.$on("messageFromFirstInput", function( event, args){
+            $scope.secondInputValue = args.messageValue;
+        })
     });

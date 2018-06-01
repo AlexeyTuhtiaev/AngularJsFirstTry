66ec3b028b13f7c6c1f4d74d11f5e6c394561abe
Alexey <alexey.tuhtiaev@gmail.com>
2018-06-01 11:20:07 +0300

    controller inheritance

---
+++ WebApplication1/Libraries/controllerInheritance.js
@@ -0,0 +1,22 @@
+﻿angular.module("calcModule", [])
+    .controller("perentCalcCtrl", function ($scope) {
+        $scope.result = {value: 0};
+        $scope.add = function () {
+            $scope.result.value = parseFloat($scope.firstValue) + parseFloat($scope.secondValue);
+        }
+        $scope.diff = function () {
+            $scope.result.value = parseFloat($scope.firstValue) - parseFloat($scope.secondValue);
+        }
+
+
+    });
+
+angular.module("calcModule")
+    .controller("childCtrl", function ($scope) {
+        $scope.mult = function () {
+            $scope.result.value = parseFloat($scope.firstValue) * parseFloat($scope.secondValue);
+        }
+        $scope.div = function () {
+            $scope.result.value = parseFloat($scope.firstValue) / parseFloat($scope.secondValue);
+        }
+    });
